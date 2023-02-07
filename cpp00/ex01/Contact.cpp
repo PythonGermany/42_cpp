eba98aaf36985bdf62e40b9c4ab631e0d9a0ebc6
PythonGermany <>
2023-02-07 02:27:55 +0100

    progress with cpp00

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -3,24 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rburgsta <rburgsta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 08:42:09 by rburgsta          #+#    #+#             */
-/*   Updated: 2023/01/03 08:59:15 by rburgsta         ###   ########.fr       */
+/*   Updated: 2023/02/06 17:46:31 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-void Contact::init()
+Contact::Contact(void) : initialized(false)
+{
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	return ;
+}
+
+static std::string get_user_input(std::string prompt)
 {
 	std::string input;
 
 	input = "\n";
 	while (input == "\n")
 	{
-		std::cout << "Enter first name: ";
-		std::cin >>input;
+		std::cout << prompt;
+		std::cin >> input;
 	}
-	this->first_name = input;
+	return input;
+}
+
+void Contact::init(void)
+{
+	this->first_name = get_user_input("Enter first name: ");
+	this->last_name = get_user_input("Enter last name: ");
+	this->nickname = get_user_input("Enter nickname: ");
+	this->phone_number = get_user_input("Enter phone number: ");
+	this->darkest_secret = get_user_input("Enter darkest secret: ");
+	this->initialized = true;
+}
+
+static void printColumn(std::string data, bool last)
+{
+	if (data.length() > 10)
+	{
+		data.erase(9, std::string::npos);
+		data.append(".");
+	}
+	for (int i = 0; i < 10 - (int)data.length(); i++)
+		std::cout << " ";
+	if (last == false)
+		std::cout << data << "|";
+	else
+		std::cout << data << std::endl;
+}
+
+void Contact::column(void)
+{
+	if (this->initialized == false)
+		return;
+	printColumn(this->first_name, false);
+	printColumn(this->last_name, false);
+	printColumn(this->nickname, true);
+}
+
+void Contact::print(void)
+{
+	if (this->initialized == false)
+		return;
+	std::cout << "First name: " << this->first_name << std::endl;
+	std::cout << "Last name: " << this->last_name << std::endl;
+	std::cout << "Nickname: " << this->nickname << std::endl;
+	std::cout << "Phone number: " << this->phone_number << std::endl;
+	std::cout << "Darkest secret: " << this->darkest_secret << std::endl;
+}
+
+bool Contact::getInitialized(void)
+{
+	return (this->initialized);
 }
--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -3,21 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rburgsta <rburgsta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 08:48:50 by rburgsta          #+#    #+#             */
-/*   Updated: 2023/01/03 08:59:45 by rburgsta         ###   ########.fr       */
+/*   Updated: 2023/02/07 02:25:49 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook()
+PhoneBook::PhoneBook(void) : index(0)
 {
-	this->index = 0;
+	return;
 }
 
-void PhoneBook::add()
+PhoneBook::~PhoneBook(void)
 {
+	return;
+}
+
+void PhoneBook::add(void)
+{
+	if (this->index >= 8)
+		this->index = 0;
 	this->contacts[this->index++].init();
+}
+
+void PhoneBook::search()
+{
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		if (this->contacts[i].getInitialized())
+		{
+			for (size_t i = 0; i < 9; i++)
+				std::cout << " ";
+			std::cout << i << "|";
+			this->contacts[i].column();
+		}
+	}
+	while (true)
+	{
+		std::cout << "Enter valid index: ";
+		std::cin >> i;
+		std::cout << "DEBUG: index: [" << i << "]" << std::endl;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else if (i >= 0 && i < 8 && this->contacts[i].getInitialized())
+			break;
+	}
+	this->contacts[i].print();
 }
