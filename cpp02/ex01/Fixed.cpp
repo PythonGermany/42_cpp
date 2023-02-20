13d133ab2cd6cf7ca21c3625d937b09a65c72588
PythonGermany <>
2023-02-20 16:10:19 +0100

    cpp02 started with ex01

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 16:02:14 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/02/17 16:02:14 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::fractionalBits = 8;
+
+Fixed::Fixed(void) : number(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int nb) : number(nb << 8)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float nb) : number((int)nb << 8)
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const& src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed& Fixed::operator=(Fixed const& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->number = rhs.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->number = raw;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->number);
+}
+
+float Fixed::toFloat(void) const
+{
+	
+	return (0);
+}
+
+int Fixed::toInt(void) const
+{
+	return (this->number >> 8);
+}
+
+std::ostream& operator<<(std::ostream& os, Fixed const& rhs)
+{
+	os << rhs.toFloat();
+	return (os);
+}
