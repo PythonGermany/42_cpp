99d07b217012be43b3d5d4757314b84c164681c3
PythonGermany <>
2023-02-22 18:19:28 +0100

    started cpp02ex02

---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 17:24:34 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/02/17 17:24:34 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+void min_test(Fixed& nb1, Fixed& nb2)
+{
+	std::cout << "min(" << nb1 << ", " << nb2 << "): " << Fixed::min(nb1, nb2) << std::endl;
+}
+
+void max_test(Fixed& nb1, Fixed& nb2)
+{
+	std::cout << "max(" << nb1 << ", " << nb2 << "): " << Fixed::max(nb1, nb2) << std::endl;
+}
+
+void min_const_test(Fixed const& nb1, Fixed const& nb2)
+{
+	std::cout << "min(" << nb1 << ", " << nb2 << "): " << Fixed::min(nb1, nb2) << std::endl;
+}
+
+void max_const_test(Fixed const &nb1, Fixed const &nb2)
+{
+	std::cout << "max(" << nb1 << ", " << nb2 << "): " << Fixed::max(nb1, nb2) << std::endl;
+}
+
+int main(void)
+{
+	Fixed a;
+	// Fixed const b(Fixed(5.05f) * Fixed(2));
+
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max(a, b) << std::endl;
+
+	Fixed c(2.254f);
+	Fixed d(0.99f);
+
+	a = Fixed(0.99f);
+	std::cout << std::endl;
+	std::cout << "Comparison operator test:" << std::endl;
+	std::cout << a << " is equal to " << c << ": " << (a == c) << std::endl;
+	std::cout << a << " is not equal to " << c << ": " << (a != c) << std::endl;
+	std::cout << a << " is smaller than " << c << ": " << (a < c) << std::endl;
+	std::cout << a << " is greater than " << c << ": " << (a > c) << std::endl;
+	std::cout << a << " is equal to / smaller than " << c << ": " << (a <= c) << std::endl;
+	std::cout << a << " is equal to / greater than " << c << ": " << (a >= c) << std::endl;
+	std::cout << a << " is equal to / smaller than " << d << ": " << (a <= d) << std::endl;
+	std::cout << a << " is equal to / greater than " << d << ": " << (a >= d) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Min/Max tests:" << std::endl;
+	min_test(a, c);
+	min_test(a, d);
+	max_test(a, c);
+	max_test(a, d);
+	std::cout << std::endl;
+
+	return 0;
+}
