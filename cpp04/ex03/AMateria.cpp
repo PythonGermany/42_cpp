87aff13e8c450d8112d5f718c5cf9551bab2d728
PythonGermany <>
2023-04-15 01:10:57 +0200

    cpp04 fixed amateria constructor

--- cpp04/ex03/AMateria.cpp
+++ cpp04/ex03/AMateria.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rburgsta <rburgsta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:39:54 by rburgsta          #+#    #+#             */
-/*   Updated: 2023/03/10 18:16:03 by rburgsta         ###   ########.fr       */
+/*   Updated: 2023/04/15 01:08:34 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ AMateria::AMateria(void)
 	std::cout << "AMateria default constructor called" << std::endl;
 }
 
-AMateria::AMateria(void) : type(type)
+AMateria::AMateria(std::string const& type) : type(type)
 {
 	std::cout << "AMateria type constructor called" << std::endl;
 }
