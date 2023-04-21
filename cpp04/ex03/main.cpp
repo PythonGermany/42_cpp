29aedb363ebeed18e418ce2c3b807b772484a9b6
PythonGermany <>
2023-04-21 14:30:20 +0200

    additional test

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rburgsta <rburgsta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:31:05 by rburgsta          #+#    #+#             */
-/*   Updated: 2023/04/21 14:23:20 by rburgsta         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:26:59 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int main()
 	src->learnMateria(ltmp);
 	delete ltmp;
 	ICharacter *me = new Character("me");
+	std::cout << me->getName() << std::endl;
 	AMateria *tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
