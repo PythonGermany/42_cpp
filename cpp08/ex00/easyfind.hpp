f3212ae0647fff1b3179fa358dc46f251a2d7635
PythonGermany <>
2023-05-08 22:28:41 +0200

    started cpp08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 20:31:52 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/05/08 20:31:52 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template <typename T>
+void easyfind(T container, int nb);
+
+# include "easyfind.tpp"
+
+#endif
