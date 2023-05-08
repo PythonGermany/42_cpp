a27cf8ae019cd86e9e93eec3df3430cf2861afb5
PythonGermany <>
2023-05-08 02:09:49 +0200

    cpp07ex01 done

---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 01:34:40 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/05/08 01:34:40 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Iter.hpp"
+
+int main()
+{
+	int intarray[] = {0, 1, 15, 17, 84, 25, 27, 18, 74, 12, 61, 61};
+	char chararray[] = "Function templates are very interesting.";
+	float floatarray[] = {1.5f, 0.5f, 10.0f, 3.3f};
+
+	iter(intarray, 12, print<int>);
+	std::cout << std::endl;
+	iter(intarray, 12, pow<int>);
+	iter(intarray, 12, print<int>);
+	std::cout << std::endl;
+	iter(intarray, 12, clear<int>);
+	iter(intarray, 12, print<int>);
+	std::cout << std::endl;
+	iter(chararray, 41, print<char>);
+	std::cout << std::endl;
+	iter(chararray, 41, toUpper<char>);
+	iter(chararray, 41, print<char>);
+	std::cout << std::endl;
+	iter(floatarray, 4, print<float>);
+	std::cout << std::endl;
+	iter(floatarray, 4, pow<float>);
+	iter(floatarray, 4, print<float>);
+	std::cout << std::endl;
+	return (0);
+}
