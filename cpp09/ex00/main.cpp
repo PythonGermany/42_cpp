bd26a0fa32d164d86f46350353a1b4759b1dac86
PythonGermany <>
2023-05-30 17:33:42 +0200

    Added correct program names and started work on cpp09ex00

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -11,8 +11,22 @@
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-int main()
+void handleError(std::string msg, int code)
 {
+	std::cerr << "Error: " << msg << "!" << std::endl;
+	std::exit(code);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		handleError("Invalid argument count", 1);
+	std::ifstream input(argv[1]);
 	
+	if (input.is_open() == false)
+		handleError("Could not open input file", 1);
 }
