4ee78c52640976931dfae87702633a8ead5d4907
PythonGermany <>
2023-07-05 13:27:01 +0200

    cpp09ex02 added list as 2nd container

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -21,7 +21,7 @@ PmergeMe::PmergeMe(PmergeMe const& rhs) { *this = rhs; }
 PmergeMe& PmergeMe::operator=(PmergeMe const& rhs) {
   if (this == &rhs) return (*this);
   vec = rhs.vec;
-  // DEV copy second container
+  lst = rhs.lst;
   return (*this);
 }
 
@@ -30,15 +30,12 @@ PmergeMe::~PmergeMe() {}
 void PmergeMe::loadSequence(std::vector<std::string>& seq) {
   if (seq.empty()) handleError("Loading: Empty sequence", 1);
   vec.clear();
-  // DEV clear second container
+  lst.clear();
   for (size_t i = 0; i < seq.size(); i++) {
-    while (seq[i].length() > 0) {
-      std::string value = seq[i].substr(0, seq[i].find(' '));
-      if (verifyValue(value)) handleError("Loading: Invalid sequence", 1);
-      vec.push_back(std::atoi(value.c_str()));
-      // DEV add to second container
-      seq[i].erase(0, value.size() + 1);
-    }
+    if (seq[i].empty()) handleError("Loading: Empty argument", 1);
+    if (verifyValue(seq[i])) handleError("Loading: Invalid sequence", 1);
+    vec.push_back(std::atoi(seq[i].c_str()));
+    lst.push_back(std::atoi(seq[i].c_str()));
   }
 }
 
@@ -56,14 +53,19 @@ void PmergeMe::sortContainerTwo() {
 }
 
 void PmergeMe::printContainerTwo() const {
-  // DEV print second container
+  std::list<int>::const_iterator itr = lst.begin();
+  while (itr != lst.end()) std::cout << *itr++ << " ";
+  std::cout << std::endl;
 }
 
 int PmergeMe::verifyValue(std::string& value) {
   if (value.empty()) return (1);
   if (value.size() > 10) return (1);
-  for (size_t i = 0; i < value.length(); i++)
+  for (size_t i = 0; i < value.length(); i++) {
+    if (i == 0 && (value[i] == '-' || value[i] == '+') && value.length() > 1)
+      continue;
     if (!std::isdigit(value[i])) return (1);
+  }
   return (0);
 }
 
