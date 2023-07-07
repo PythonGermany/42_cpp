0058529c296412e3ec488c97168e340f050789bf
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:33 +0000

    cpp01 ex05and06 mostly done

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,22 +6,45 @@
 /*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 11:49:44 by rburgsta          #+#    #+#             */
-/*   Updated: 2023/02/17 10:07:51 by rburgsta         ###   ########.fr       */
+/*   Updated: 2023/02/17 15:32:14 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main()
+void filter(Harl *h, int i, std::string* lvls)
+{
+	for (; i < 4; i++)
+		h->complain(lvls[i]);
+}
+
+int main(int argc, char **argv)
 {
 	Harl h;
+	int i;
+	std::string filterLVL[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	h.complain("DEBUG");
-	std::cout << std::endl;
-	h.complain("ERROR");
-	std::cout << std::endl;
-	h.complain("INFO");
-	std::cout << std::endl;
-	h.complain("WARNING");
+	if (argc != 2)
+		return (std::cout << "use: " << argv[0] << " [filter level]" << std::endl, 1);
+	for (i = 0; i < 4; i++)
+		if (!filterLVL[i].compare(argv[1]))
+			break;
+	switch (i)
+	{
+	case 0:
+		filter(&h, i, filterLVL);
+		break;
+	case 1:
+		filter(&h, i, filterLVL);
+		break;
+	case 2:
+		filter(&h, i, filterLVL);
+		break;
+	case 3:
+		filter(&h, i, filterLVL);
+		break;
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 	return (0);
 }
