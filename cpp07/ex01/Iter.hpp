b82e8be9c02e9750311f76778fda72bcd4bb0d5e
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:35 +0000

    cpp07ex01 done

---
+++ cpp07/ex01/Iter.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 01:27:53 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/05/08 01:27:53 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+# include <typeinfo>
+
+template <typename T>
+void print(T& a)
+{
+	std::cout << a << " ";
+}
+
+template <typename T>
+void clear(T& a)
+{
+	a = 0;
+}
+
+template <typename T>
+void pow(T& a)
+{
+	a *= a;
+}
+
+template <typename T>
+void toUpper(T& a)
+{
+	if (typeid(a) == typeid(char))
+	{
+		if (a >= 'a' && a <= 'z')
+			a -= 'a' - 'A';
+	}
+}
+
+template <typename Tp, typename T>
+void iter(Tp array[], int length, void (*f)(T))
+{
+	for (int i = 0; i < length; i++)
+		f(array[i]);
+}
+
+#endif
