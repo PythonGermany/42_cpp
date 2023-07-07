e8e42bf25fc467e0dd5bf36fd635f0ce0fb364a6
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:35 +0000

    cpp07ex00 done

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 01:10:00 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/05/08 01:10:00 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T& min(T& a, T&b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T& max(T& a, T&b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
