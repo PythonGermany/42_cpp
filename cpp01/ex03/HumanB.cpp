3359264659e239fffc2f47e77cd42aeff1b72918
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:33 +0000

    cpp01ex03 simplified

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -24,7 +24,7 @@ HumanB::~HumanB(void)
 
 void HumanB::attack(void)
 {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	std::cout << this->name << " attacks with their " << weapon->getType() << std::endl;
 }
 
 void HumanB::setWeapon(Weapon& weapon)
