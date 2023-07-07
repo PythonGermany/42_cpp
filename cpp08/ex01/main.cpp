323f7311bdba861c47fc0eaa41dd2f20c1996d6c
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:36 +0000

    cpp08ex01 addded new and fixed old tests

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -26,21 +26,22 @@ int main() {
   sp.addNumber(17);
   sp.addNumber(9);
   sp.addNumber(11);
-  std::cout << sp.shortestSpan() << std::endl;
-  std::cout << sp.longestSpan() << std::endl << std::endl;
+
+  std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp.longestSpan() << std::endl << std::endl;
 
   Span sp2(sp);
-  std::cout << sp2.shortestSpan() << std::endl;
-  std::cout << sp2.longestSpan() << std::endl << std::endl;
+  std::cout << "Shortest span: " << sp2.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp2.longestSpan() << std::endl << std::endl;
 
   Span sp3(20000);
   try {
-    std::cout << sp3.shortestSpan() << std::endl;
+    std::cout << "Shortest span: " << sp3.shortestSpan() << std::endl;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
   try {
-    std::cout << sp3.longestSpan() << std::endl;
+    std::cout << "Longest span: " << sp3.longestSpan() << std::endl;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
@@ -50,13 +51,13 @@ int main() {
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
-  std::cout << sp3.shortestSpan() << std::endl;
-  std::cout << sp3.longestSpan() << std::endl << std::endl;
+  std::cout << "Shortest span: " << sp3.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp3.longestSpan() << std::endl << std::endl;
 
   Span sp4(50000);
   for (int i = 0; i < 50000; i++) sp4.addNumber(rand());
-  std::cout << sp4.shortestSpan() << std::endl;
-  std::cout << sp4.longestSpan() << std::endl << std::endl;
+  std::cout << "Shortest span: " << sp4.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp4.longestSpan() << std::endl << std::endl;
 
   Span sp5(50000);
   std::vector<int> vec(25000, 0);
@@ -66,15 +67,16 @@ int main() {
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
-  std::cout << sp5.shortestSpan() << std::endl;
-  std::cout << sp5.longestSpan() << std::endl;
+  std::cout << "Shortest span: " << sp5.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp5.longestSpan() << std::endl;
   vec.push_back(rand());
+  std::generate(vec.begin(), vec.end(), rand);
   try {
     sp5.addRange(vec.begin(), vec.end());
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
-  std::cout << sp5.shortestSpan() << std::endl;
-  std::cout << sp5.longestSpan() << std::endl << std::endl;
+  std::cout << "Shortest span: " << sp5.shortestSpan() << std::endl;
+  std::cout << "Longest span: " << sp5.longestSpan() << std::endl << std::endl;
   return 0;
 }
