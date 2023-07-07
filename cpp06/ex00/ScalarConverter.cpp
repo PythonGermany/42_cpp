60f93e34bb7912e8e5217f36ff66fc461ea731bc
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:34 +0000

    Continued work on cpp06

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 23:36:22 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/05/06 23:36:22 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+	std::cout << "ScalarConverter default constructor called" << std::endl;
+}
+
+ScalarConverter::ScalarConverter(ScalarConverter const& rhs)
+{
+	std::cout << "ScalarConverter copy constructor called" << std::endl;
+	*this = rhs;
+}
+
+ScalarConverter& ScalarConverter::operator=(ScalarConverter const& rhs)
+{
+	std::cout << "ScalarConverter copy assignment operator called" << std::endl;
+	
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter()
+{
+	std::cout << "ScalarConverter destructor called" << std::endl;
+}
