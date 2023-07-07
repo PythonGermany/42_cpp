6dd2c8111ca17a2f979cd99ae37a84e3dfb4c446
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:39:33 +0000

    cpp00 added

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 08:09:09 by rburgsta          #+#    #+#             */
+/*   Updated: 2023/01/03 08:59:50 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook
+{
+private:
+	Contact contacts[8];
+	int index;
+public:
+	PhoneBook();
+	~PhoneBook();
+
+	void add();
+};
+
+#endif
