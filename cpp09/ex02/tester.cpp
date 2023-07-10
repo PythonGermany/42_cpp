35c855cc04d98f3cd2de6114034fa2d2205e7c69
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-10 18:34:19 +0000

    cpp09ex02 better tester

--- cpp09/ex02/tester.cpp
+++ cpp09/ex02/tester.cpp
@@ -18,39 +18,50 @@
 
 #include "PmergeMe.hpp"
 
-int main(int argc, char **argv) {
-  if (argc != 3) return 1;
+template <typename T>
+void runTest(size_t range, size_t iterations) {
   int arr[] = {0,  1,  3,  5,  7,  10, 13, 16, 19, 22, 26,
                30, 34, 38, 42, 46, 50, 54, 58, 62, 66, 71};
   std::vector<int> worst_case(std::begin(arr), std::end(arr));
-  int sizes = std::atoi(argv[1]);
-  int iterations = std::atoi(argv[2]);
-  PmergeMe p;
-  bool error = false;
   std::random_device rd;
   std::mt19937 g(rd());
-  std::vector<int> vec;
+  PmergeMe p;
+  T data;
 
-  for (int i = 0; !error && i < sizes; i++) {
-    vec.push_back(i + 1);
-    for (int k = 0; !error && k < iterations; k++) {
-      std::vector<int> temp = vec;
+  for (int i = 0; i < range; i++) {
+    data.push_back(i);
+    for (int k = 0; k < iterations; k++) {
+      T temp = data;
       std::shuffle(temp.begin(), temp.end(), g);
-      p.mergeInsertSort<std::vector<int> >(temp.begin(), temp.end(), 1);
+      p.mergeInsertSort<T>(temp.begin(), temp.end(), 1);
       if (i < worst_case.size() && worst_case[i] < p.compCount) {
         std::cout << "Error for size " << i + 1
                   << " comparisions: " << p.compCount
                   << " max: " << worst_case[i] << " Output: ";
         p.printContainer(temp);
         std::cout << std::endl;
-        error = true;
+        std::exit(1);
       }
-      if (temp != vec) {
+      if (temp != data) {
         std::cout << "Error: Wrong output: ";
         p.printContainer(temp);
-        error = true;
+        std::exit(1);
       }
       p.compCount = 0;
     }
   }
+}
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " <range> <iterations>" << std::endl;
+    return 1;
+  }
+  int range = std::atoi(argv[1]);
+  int iterations = std::atoi(argv[2]);
+
+  runTest<std::vector<int>>(range, iterations);
+  std::cout << "std::vector<int> OK" << std::endl;
+  runTest<std::deque<int>>(range, iterations);
+  std::cout << "std::deque<int> OK" << std::endl;
 }
